a180a8b1fdb8e35576502577d421049d5f4b63be
Temi Tope <jejetemmy8@gmail.com>
2025-11-16 14:06:03 +0100

    Add Mailgun HTTP API email backend to bypass SMTP blocking

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -43,17 +43,14 @@ CSRF_TRUSTED_ORIGINS = [
     'https://*.railway.app',
 ]
 
-# Email Configuration - GMAIL SMTP
-# This will send actual emails to Gmail inbox
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = 'JejeHub <Jejetemmy8@gmail.com>'
-SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'Jejetemmy8@gmail.com')
-EMAIL_TIMEOUT = 10 
+# Email Configuration - Mailgun HTTP API
+# Uses HTTP API instead of SMTP to avoid port blocking on Render
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+MAILGUN_API_KEY = os.environ.get('MAILGUN_API_KEY', '')
+MAILGUN_DOMAIN = os.environ.get('MAILGUN_DOMAIN', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'JejeHub <jejetemmy8@gmail.com>')
+SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'jejetemmy8@gmail.com')
+EMAIL_TIMEOUT = 10
 
 # Optional: Uncomment below to test emails in console instead of sending to Gmail
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
